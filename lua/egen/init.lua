3f6efe5466a9aa98ef676b885ff9af5c608a17d7
Mehdi Benmessaoud <mehdi.benmessaoud@cgi.com>
2023-12-11 15:53:52 +0200

    Neovim own configuration

---
+++ lua/egen/init.lua
@@ -0,0 +1,3 @@
+require("egen.remap")
+require("egen.lazy")
+require("egen.set")
---
+++ lua/egen/lazy.lua
@@ -0,0 +1,38 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{ 'rose-pine/neovim', name = 'rose-pine' },
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{'nvim-treesitter/playground'},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+	},
+	{
+		'mbbill/undotree'
+	}, 
+	{ 
+		'tpope/vim-fugitive'
+	},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+})
